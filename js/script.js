649bc5d2d49b9b1398d58fc35558eecc01fb30ba
Dessi-arg <francodessana@gmail.com>
2023-03-14 20:57:08 -0300

    PreEntrega2

--- js/script.js
+++ js/script.js
@@ -1,31 +1,102 @@
+//clase producto y sus metodos 
+class Producto{
+    constructor(nombre, precio, stock){
+        
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
 
-function precioFinal() {
-    let precioProducto = 0
+    datosProd(){
+        console.log("Nombre: ", this.nombre);
+        console.log("Precio: ", this.precio);
+        console.log("Stock: ", this.stock);
+    }
 
-    while (precioProducto <= 0) {
-        precioProducto = parseFloat(prompt("Ingrese el valor de la compra en $ARS"));
-        if (precioProducto <= 0) {
-        alert("El valor ingresado es inválido. Por favor, ingrese un valor mayor que 0.");
+    stockProd( unidades ){
+        if( this.stock >= unidades ){
+            this.stock = this.stock - unidades;
+            return true
+        }else{
+            return false;
         }
-}
-
-    let impuesto = precioProducto * 0.75;
-    let valorTotal = precioProducto + impuesto;
-    alert("El valor FINAL es: $" + valorTotal + " y el total de impuestos es de ARS " + impuesto);
-
-    let repetir = prompt("¿Desea calcular el precio final de otra compra? (S/N)");
-    switch (repetir) {
-        case "S":
-        precioFinal();
-        break;
-        case "N":
-        alert("Gracias, Vuelva Pronto!");
-        break;
-        default:
-        alert("Opción inválida. Por favor, seleccione S o N , en mayúsculas.");
-        precioFinal();
-        break;
     }
 }
 
-precioFinal();
+//ARMADO DE LISTA Y ALMACENAMIENTO DE OBJETOS EN LA MISMA
+
+let listaProductos = [];
+
+for( let i = 0 ; i < 3 ; i = i + 1){
+    let nombre = prompt('Ingrese el nombre del producto');
+    let precio = prompt('Ingrese el precio del producto');
+    let stock = prompt('Ingrese el stock del producto');
+
+    let producto = new Producto(nombre, precio, stock);
+
+    listaProductos.push(producto);
+}
+
+
+console.log("Lista de productos")
+
+for (let producto of listaProductos) {
+    producto.datosProd();
+    
+}
+
+//FUNCION PARA REALIZAR UNA COMPRA
+
+function comprarProducto(producto) {
+
+    let unidades = prompt("¿Cúantas unidades desea comprar?");
+
+    if (producto.stockProd(unidades)) {
+        console.log("¡¡ Gracias por su compra !!");
+        return true;
+    } else {
+        console.log("¡¡ No puedes comprar ese producto !! stock insuficiente ");
+        console.log("Stock disponible: ", producto.stock);
+        return false;
+    }
+}
+
+//CICLO PARA QUE EL USUARIO SIGA COMPRANDO HASTA QUE INDIQUE "NO"
+let seguirComprando = true;
+while (seguirComprando) {
+
+    let compraUser = prompt('Ingrese el producto que desea comprar o escriba "NO" para finalizar la compra');
+    if (compraUser.toUpperCase() === "NO") {
+        seguirComprando = false;
+        console.log("¡¡ Gracias por su compra !!");
+        break;
+    }
+
+    let resultadoFind = listaProductos.find((producto) => producto.nombre === compraUser);
+
+    if (resultadoFind !== undefined) {
+        console.log(resultadoFind);
+
+        let compraExitosa = comprarProducto(resultadoFind);
+
+        if (!compraExitosa) {
+            continue; // si no se pudo realizar la compra, se vuelve a preguntar por el producto
+        }
+
+    } else {
+        console.log("No existe el producto", compraUser);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
